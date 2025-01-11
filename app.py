fc5f9a14da56bb4ff525eb01e8a458a68104bbc3
SohamShrivastava <sohamshrivastava0935@gmail.com>
2025-01-11 18:33:18 +0530

    Add course management features: implement course addition and deletion

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import json
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash
-
+from flask import Flask, render_template, request, redirect, url_for, flash
 # Flask App Initialization
 app = Flask(__name__)
 app.secret_key = 'secret'
 COURSE_FILE = 'course_catalog.json'
-
 
 # Utility Functions
 def load_courses():
@@ -45,7 +44,36 @@ def course_details(code):
         return redirect(url_for('course_catalog'))
     return render_template('course_details.html', course=course)
 
+@app.route('/save_course', methods=['post'])
+def save_course():
+    courses= load_courses()
+    name = request.form['name']
+    code = request.form['code']
+    description = request.form['description']
+    instructor = request.form['instructor']
+    new_course = {
+        'name': name,
+        'code': code,
+        'description': description,
+        'instructor': instructor
+    }
+    save_courses(new_course) 
+    
+    return redirect(url_for('course_catalog'))
 
+@app.route('/delete_course/<code>', methods=['POST'])
+def delete_course(code):
+    courses = load_courses()  
+    courses = [course for course in courses if course['code'] != code] 
+    with open(COURSE_FILE, 'w') as file:
+        json.dump(courses, file, indent=4)
+    flash(f"Course with code '{code}' has been deleted.", "success")
+    return redirect(url_for('course_catalog'))
+
+@app.route('/add_course')
+def add_course():
+        return render_template('add_course.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
